d98958ec3f28208d53507e70f8b7b2f15e8fba87
cosyrusty <priyaanyadav@gmail.com>
2023-04-15 15:15:59 +0530

    player movement and enemies spawning

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,37 @@
-use bevy::{prelude::*, window::PrimaryWindow};
+use bevy::{math::vec3, prelude::*, window::PrimaryWindow};
+use rand::prelude::*;
+
+pub const PLAYER_SPEED: f32 = 500.0;
+pub const PLAYER_SIZE: f32 = 64.0; // player sprite size
+pub const ENEMY_COUNT: usize = 5;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(spawn_player)
+        .add_startup_system(spawn_enemies)
         .add_startup_system(spawn_camera)
+        .add_system(player_movement)
+        .add_system(confine_player_movement)
         .run()
 }
 
 #[derive(Component)]
 pub struct Player;
 
+#[derive(Component)]
+pub struct Enemy;
+
 pub fn spawn_player(
-    mut cmds: Commands,
+    mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
     asset_server: Res<AssetServer>,
 ) {
     // we know that there is only one primary window hence get_single()
     let window = window_query.get_single().unwrap();
 
-    cmds.spawn((
+    commands.spawn((
+        // Bundle is pack of components. where trasform is one of component.
         SpriteBundle {
             transform: Transform::from_xyz(window.width() / 2., window.height() / 2., 0.0),
             // asset_server automatically look in assets directory
@@ -30,15 +42,81 @@ pub fn spawn_player(
     ));
 }
 
-pub fn spawn_camera(mut cmds: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+pub fn spawn_enemies(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
     // we know that there is only one primary window hence get_single()
     let window = window_query.get_single().unwrap();
 
-    cmds.spawn((
-        Camera2dBundle {
-            transform: Transform::from_xyz(window.width() / 2., window.height() / 2., 0.0),
-            ..default()
-        },
-        Player,
-    ));
+    for _ in 0..ENEMY_COUNT {
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprite/ball_red_large.png"),
+                ..default()
+            },
+            Enemy,
+        ));
+    }
+}
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    // we know that there is only one primary window hence get_single()
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2., window.height() / 2., 0.0),
+        ..default()
+    });
+}
+
+pub fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+        if keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up) {
+            direction.y = 1.;
+        }
+        if keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down) {
+            direction.y = -1.;
+        }
+        if keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left) {
+            direction.x = -1.;
+        }
+        if keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right) {
+            direction.x = 1.;
+        }
+
+        // in case direction is zero we should not normalize hence
+        direction = direction.normalize_or_zero();
+
+        transform.translation += direction * (PLAYER_SPEED * time.delta_seconds());
+    }
+}
+
+pub fn confine_player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+
+        let player_radius = PLAYER_SIZE / 2.;
+        let min = vec3(player_radius, player_radius, 0.);
+        let max = vec3(
+            window.width() - player_radius,
+            window.height() - player_radius,
+            0.,
+        );
+
+        transform.translation = transform.translation.clamp(min, max);
+    }
 }
